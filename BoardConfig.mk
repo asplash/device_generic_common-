721e6199f70e54aa070de864412f30f0bb02549c
asplash <asplash@hotmail.fr>
2016-02-20 10:17:06 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 WPA_SUPPLICANT_VERSION := VER_2_1_DEVEL
 WIFI_DRIVER_MODULE_NAME := brcmfmac
 
-BOARD_GPU_DRIVERS ?= i915 i965 nouveau r300g r600g swrast virgl
+BOARD_GPU_DRIVERS ?= i915
 ifneq ($(strip $(BOARD_GPU_DRIVERS)),)
 TARGET_HARDWARE_3D := true
 BOARD_EGL_CFG ?= device/generic/common/gpu/egl_mesa.cfg
